03b82774322baa1029238552c0aebbcff4f70039
Alexander Zhukov <zuav@crystax.net>
2014-10-31 22:49:05 +0300

    Initial commit

---
+++ lib/gcc/x86_64-apple-darwin12.5.0/4.9.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.1/configure --with-pkgversion='Crystax NDK 10' --prefix=/Users/zuav/src/gcc/x86_64-apple-darwin-4.9.1 --enable-checking=release --program-prefix=x86_64-apple-darwin12- --disable-libgomp --with-build-sysroot=/Users/zuav/src/ndk/platform/prebuilts/sysroot/darwin-x86/MacOSX10.6.sdk --with-gmp=/usr/local/Cellar/gmp/6.0.0a --with-mpfr=/usr/local/Cellar/mpfr/3.1.2-p10 --with-mpc=/usr/local/Cellar/libmpc/1.0.2 --with-isl=/usr/local/Cellar/isl/0.12.2 --with-cloog=/usr/local/Cellar/cloog/0.18.1 --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
